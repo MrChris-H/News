210e4a2697a680c41297e5da5ccdcdaec274468c
MrChris-H <christopher.hopkins998@gmail.com>
2022-02-14 14:22:05 +0000

    create green route controller/method for /api/topics

---
+++ controllers/topics-controller.js
@@ -0,0 +1,11 @@
+const { fetchTopics } = require("../models/topics-methods");
+
+exports.getTopics = (req, res) => {
+  fetchTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
