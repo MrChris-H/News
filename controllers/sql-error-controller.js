a3e7dbd350f79e00632ccbedacb214906fd8233c
MrChris-H <christopher.hopkins998@gmail.com>
2022-02-15 11:40:38 +0000

    complete tests for patch api/article/id

--- controllers/sql-error-controller.js
+++ controllers/sql-error-controller.js
@@ -1,5 +1,5 @@
 exports.sqlErr = (err, req, res, next) => {
   if (err.code === "22P02") {
-    res.status(400).send({ msg: "invalid input type" });
+    res.status(400).send({ msg: "bad request" });
   } else next(err);
 };
--- models/articles-models.js
+++ models/articles-models.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 
 exports.fetchArticle = (id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .query("SELECT * FROM articles WHERE article_id = $1;", [id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "article does not exist" });
@@ -19,7 +19,7 @@ exports.updateArticle = (votes, id) => {
   SET votes = $1
   WHERE article_id = $2 
   RETURNING*
-  `,
+  ;`,
       [votes, id]
     )
     .then(({ rows }) => {
