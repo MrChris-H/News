a03799649843439df25ce2eaa69315ecb08d01e2
MrChris-H <christopher.hopkins998@gmail.com>
2022-02-17 12:58:19 +0000

    condensed customErr to remove conflict

--- controllers/errors/custom-error-controllers.js
+++ controllers/errors/custom-error-controllers.js
@@ -1,14 +1,5 @@
 exports.customErr = (err, req, res, next) => {
-  if (err.status === 404 && err.msg === "resource not found") {
-    res.status(404).send({ msg: "resource not found" });
-  } else if (err.status === 400 && err.msg === "invalid sort query") {
-    res.status(400).send({ msg: "invalid sort query" });
-  } else if (err.status === 400 && err.msg === "invalid order query") {
-    res.status(400).send({ msg: "invalid order query" });
-  } else if (
-    err.status === 404 &&
-    err.msg === "no articles found for this topic"
-  ) {
-    res.status(404).send({ msg: "no articles found for this topic" });
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else next(err);
 };
