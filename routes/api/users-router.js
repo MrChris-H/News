3203551eee8b9bf1f61ef499bf1c5639873aa07c
MrChris-H <christopher.hopkins998@gmail.com>
2022-02-18 15:50:05 +0000

    deleted wor for other ticket

--- routes/api/users-router.js
+++ routes/api/users-router.js
@@ -3,6 +3,5 @@ const { getUsers, getUser } = require("../../controllers/users-controllers");
 const usersRouter = require("express").Router();
 
 usersRouter.get("/", getUsers);
-usersRouter.get("/:username", getUser);
 
 module.exports = { usersRouter };
