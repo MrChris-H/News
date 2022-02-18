f924dbf1b6b1feb9c3c351904ee6d4d95f7d4fef
MrChris-H <christopher.hopkins998@gmail.com>
2022-02-18 14:54:23 +0000

    create router for /api/users

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/api", apiRouter);
 // app.get(`/api/topics`, getTopics);
 // app.get(`/api/articles/:article_id`, getArticle);
 // app.patch(`/api/articles/:article_id`, patchArticle);
-app.get(`/api/users`, getUsers);
+// app.get(`/api/users`, getUsers);
 // app.get(`/api/articles`, getArticles);
 // app.get(`/api/articles/:article_id/comments`, getCommentsByArticleId);
 // app.post(`/api/articles/:article_id/comments`, postCommentByArticleId);
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,10 +1,14 @@
 const { endPoints } = require("../controllers/api-controllers");
 const { articlesRouter } = require("./api/articles-router");
 const { topicsRouter } = require("./api/topics-router");
+const { usersRouter } = require("./api/users-router");
 
 const apiRouter = require("express").Router();
 
 apiRouter.get("/", endPoints);
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/articles", articlesRouter);
+apiRouter.use("/users", usersRouter);
+// apiRouter.use("")
+
 module.exports = { apiRouter };
