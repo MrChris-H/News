2e1b6401c9664af8bc2e23f6aa9e9124382d68b1
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-07 11:08:56 +0000

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { customErr } = require("./controllers/errors/custom-error-controllers");
 const {
   badPath,
@@ -8,13 +9,14 @@ const { sqlErr } = require("./controllers/errors/sql-error-controllers");
 const { apiRouter } = require("./routes/api-router");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
 //--------------------------------------------------------------------------
 app.all("/*", badPath); // 404 path not found
 //--------------------------------------------------------------------------
-app.use(sqlErr);  // sql related errors
+app.use(sqlErr); // sql related errors
 app.use(customErr); // custom errors
 app.use(serverErr); // 500 internal server error
 
