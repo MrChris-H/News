c5a84b220ebb9d5f6bc014753342d0883448271b
MrChris-H <christopher.hopkins998@gmail.com>
2022-03-08 19:58:16 +0000

    add feature back that wasn't merged correctly

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -617,6 +617,33 @@ describe("The Server", () => {
       });
     });
   });
+  describe("/api/users/:username", () => {
+    describe("GET", () => {
+      it("Status 200, responds with an object containing username, avatar_url and name", () => {
+        return request(app)
+          .get("/api/users/rogersop")
+          .expect(200)
+          .then(({ body: { user } }) => {
+            expect(user).toEqual(
+              expect.objectContaining({
+                username: "rogersop",
+                avatar_url:
+                  "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
+                name: "paul",
+              })
+            );
+          });
+      });
+      it("Status 404, when a username that is valid but does not exist is input", () => {
+        return request(app)
+          .get("/api/users/not_a_username")
+          .expect(404)
+          .then(({ body: { msg } }) => {
+            expect(msg).toBe("resource not found");
+          });
+      });
+    });
+  });
   describe("/api/articles/ (queries)", () => {
     describe(".GET", () => {
       it("Status 200, allow for client to sort by a valid column ", () => {
--- models/users-models.js
+++ models/users-models.js
@@ -6,3 +6,10 @@ exports.fetchUsers = () => {
   });
 };
 
+exports.fetchUser = (username) => {
+  return db
+    .query("SELECT * FROM users WHERE username=$1;", [username])
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
